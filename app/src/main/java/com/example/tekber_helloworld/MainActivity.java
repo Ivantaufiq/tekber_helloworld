ca6997fab5e78f2d2af7160ab1ec8dbf9a5e4bd2
taufiq.ivan15@student.untan.ac.id <taufiq.ivan15@student.untan.ac.id>
2019-09-19 21:58:39 +0700

    update tugas pertama hitung dan toast message

--- app/src/main/java/com/example/tekber_helloworld/MainActivity.java
+++ app/src/main/java/com/example/tekber_helloworld/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.tekber_helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int naik = 0;
+    Button btnhitung, btntoast;
+    TextView texthasil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btntoast = findViewById(R.id.btntoast);
+        btnhitung = findViewById(R.id.btnhitung);
+        texthasil = findViewById(R.id.texthasil);
+
+        btntoast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello Toast!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnhitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                naik ++;
+                texthasil.setText(Integer.toString(naik));
+            }
+        });
     }
 }
